25a75f469b37e5eeb3e29e088f1080b5120bd8b4
Jeremia Axel <jeremiaaxelb@gmail.com>
2023-04-26 19:44:56 +0700

    update on review
    gitignore: move pycache to root
    
    app.be: concise ChangeMode

--- app/be/src/dto/change_mode.py
+++ app/be/src/dto/change_mode.py
@@ -4,12 +4,4 @@ from util.enums import AppMode
 
 
 class ChangeMode(BaseModel):
-    current_mode: str = Field(
-        description="Ai or Override",
-    )
-
-    @validator('current_mode')
-    def mode_validator(cls, mode):
-        if mode not in AppMode:
-            raise ValueError('Mode must be Ai or Override')
-        return mode
+    current_mode: AppMode
